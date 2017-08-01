0146abc7af7f9c42ef3a172660031db0462d1aa5
Mason Pomeroy <Mason.Pomeroy@D2L.com>
2017-08-01 16:39:30 -0400

    Removed burden of validating DefinedEntities from the user.

--- ksiren/src/main/java/com/brightspace/ksiren/DefinedEntity.kt
+++ ksiren/src/main/java/com/brightspace/ksiren/DefinedEntity.kt
@@ -15,6 +15,9 @@ package com.brightspace.ksiren
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-interface DefinedEntity {
-	fun getKSirenSignature(): KSirenEntitySignature
+abstract class DefinedEntity(entity: Entity, signature: KSirenEntitySignature) {
+	init {
+		//Will throw a ValidationException if the Entity passed does not conform to the signature.
+		signature.validate(entity)
+	}
 }
