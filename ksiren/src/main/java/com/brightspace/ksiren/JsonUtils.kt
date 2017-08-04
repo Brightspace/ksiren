a92919e82200ce1ce61b6ab6b0141b86a771cb77
Eduardo Barrenechea <eduardo.barrenechea@d2l.com>
2017-08-04 14:26:00 -0400

    Use an object declaration in `JsonUtils`

--- ksiren/src/main/java/com/brightspace/ksiren/JsonUtils.kt
+++ ksiren/src/main/java/com/brightspace/ksiren/JsonUtils.kt
@@ -15,112 +15,93 @@ package com.brightspace.ksiren
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-class JsonUtils {
-	companion object {
-		fun toJson(entity: Entity): CharSequence {
-			val builder = StringBuilder()
-			builder.append("{ ")
+object JsonUtils {
+	fun toJson(entity: Entity): CharSequence {
+		val builder = StringBuilder()
+		builder.append("{ ")
 
-			addArray("class", entity.classes, false, builder)
+		addArray("class", entity.classes, false, builder)
 
-			addArray("rel", entity.rel, true, builder)
+		addArray("rel", entity.rel, true, builder)
 
-			if (!entity.properties.isEmpty()) {
-				builder.append(", \"properties\": { ")
+		if (!entity.properties.isEmpty()) {
+			builder.append(", \"properties\": { ")
 
-				var count = 0
-				entity.properties.forEach { (name, value) ->
-					addProperty(name, value, count > 0, builder)
-					count += 1
+			var count = 0
+			entity.properties.forEach { (name, value) ->
+				addProperty(name, value, count > 0, builder)
+				count += 1
+			}
+
+			builder.append(" }")
+		}
+
+		addJsonSerializable("entities", entity.entities, builder)
+		addJsonSerializable("actions", entity.actions.values, builder)
+		addJsonSerializable("links", entity.links, builder)
+
+		builder.append(" }")
+
+		return builder.toString()
+	}
+
+	fun toJson(link: Link): CharSequence {
+		val builder = StringBuilder()
+		builder.append("{ ")
+
+		addArray("rel", link.rels, false, builder)
+		addArray("class", link.classes, true, builder)
+		addProperty("title", link.title, true, builder)
+		addProperty("type", link.type, true, builder)
+		addProperty("href", link.href, true, builder)
+		builder.append(" }")
+
+		return builder.toString()
+	}
+
+	fun toJson(action: Action): CharSequence {
+		val builder = StringBuilder()
+		builder.append("{ ")
+
+		addProperty("name", action.name, false, builder)
+		addProperty("title", action.title, true, builder)
+		addArray("classes", action.classes, true, builder)
+		addProperty("method", action.method, true, builder)
+		addProperty("href", action.href, true, builder)
+		addProperty("type", action.type.value, true, builder)
+
+		if (!action.fields.isEmpty()) {
+			builder.append(", \"fields\": [")
+			action.fields.forEachIndexed { index, field ->
+				if (index > 0) {
+					builder.append(", ")
 				}
 
-				builder.append(" }")
+				builder.append(toJson(field))
 			}
-
-			addJsonSerializable("entities", entity.entities, builder)
-			addJsonSerializable("actions", entity.actions.values, builder)
-			addJsonSerializable("links", entity.links, builder)
-
-			builder.append(" }")
-
-			return builder.toString()
+			builder.append("]")
 		}
 
-		fun toJson(link: Link): CharSequence {
-			val builder = StringBuilder()
-			builder.append("{ ")
+		builder.append(" }")
 
-			addArray("rel", link.rels, false, builder)
-			addArray("class", link.classes, true, builder)
-			addProperty("title", link.title, true, builder)
-			addProperty("type", link.type, true, builder)
-			addProperty("href", link.href, true, builder)
-			builder.append(" }")
+		return builder.toString()
+	}
 
-			return builder.toString()
-		}
+	fun toJson(field: Field): CharSequence {
+		val builder = StringBuilder()
+		builder.append("{ ")
 
-		fun toJson(action: Action): CharSequence {
-			val builder = StringBuilder()
-			builder.append("{ ")
+		addProperty("name", field.name, false, builder)
+		addArray("class", field.classes, true, builder)
+		addProperty("type", field.type, true, builder)
+		addProperty("value", field.value, true, builder)
 
-			addProperty("name", action.name, false, builder)
-			addProperty("title", action.title, true, builder)
-			addArray("classes", action.classes, true, builder)
-			addProperty("method", action.method, true, builder)
-			addProperty("href", action.href, true, builder)
-			addProperty("type", action.type.value, true, builder)
+		builder.append(" }")
+		return builder.toString()
+	}
 
-			if (!action.fields.isEmpty()) {
-				builder.append(", \"fields\": [")
-				action.fields.forEachIndexed { index, field ->
-					if (index > 0) {
-						builder.append(", ")
-					}
-
-					builder.append(toJson(field))
-				}
-				builder.append("]")
-			}
-
-			builder.append(" }")
-
-			return builder.toString()
-		}
-
-		fun toJson(field: Field): CharSequence {
-			val builder = StringBuilder()
-			builder.append("{ ")
-
-			addProperty("name", field.name, false, builder)
-			addArray("class", field.classes, true, builder)
-			addProperty("type", field.type, true, builder)
-			addProperty("value", field.value, true, builder)
-
-			builder.append(" }")
-			return builder.toString()
-		}
-
-		private fun addProperty(name: String, value: String?, prependComma: Boolean, builder: StringBuilder) {
-			value?.let {
-				if (prependComma) {
-					builder.append(", \"")
-				} else {
-					builder.append("\"")
-				}
-
-				builder.append(name)
-				builder.append("\": \"")
-				builder.append(it)
-				builder.append("\"")
-			}
-		}
-
-		fun addArray(name: String, array: List<String>, prependComma: Boolean, builder: StringBuilder) {
-			if (array.isEmpty()) {
-				return
-			}
-
+	private fun addProperty(name: String, value: String?, prependComma: Boolean, builder: StringBuilder) {
+		value?.let {
 			if (prependComma) {
 				builder.append(", \"")
 			} else {
@@ -128,33 +109,50 @@ class JsonUtils {
 			}
 
 			builder.append(name)
-			builder.append("\": [ ")
-			array.forEachIndexed { index, value ->
+			builder.append("\": \"")
+			builder.append(it)
+			builder.append("\"")
+		}
+	}
+
+	fun addArray(name: String, array: List<String>, prependComma: Boolean, builder: StringBuilder) {
+		if (array.isEmpty()) {
+			return
+		}
+
+		if (prependComma) {
+			builder.append(", \"")
+		} else {
+			builder.append("\"")
+		}
+
+		builder.append(name)
+		builder.append("\": [ ")
+		array.forEachIndexed { index, value ->
+			if (index > 0) {
+				builder.append(", ")
+			}
+
+			builder.append("\"")
+			builder.append(value)
+			builder.append("\"")
+		}
+		builder.append(" ]")
+	}
+
+	private fun addJsonSerializable(name: String, items: Collection<JsonSerializable>, builder: StringBuilder) {
+		if (!items.isEmpty()) {
+			builder.append(", \"").append(name).append("\": [")
+
+			items.forEachIndexed { index, item ->
 				if (index > 0) {
 					builder.append(", ")
 				}
 
-				builder.append("\"")
-				builder.append(value)
-				builder.append("\"")
+				builder.append(item.toJson())
 			}
-			builder.append(" ]")
-		}
 
-		private fun addJsonSerializable(name: String, items: Collection<JsonSerializable>, builder: StringBuilder) {
-			if (!items.isEmpty()) {
-				builder.append(", \"").append(name).append("\": [")
-
-				items.forEachIndexed { index, item ->
-					if (index > 0) {
-						builder.append(", ")
-					}
-
-					builder.append(item.toJson())
-				}
-
-				builder.append("]")
-			}
+			builder.append("]")
 		}
 	}
 }
